a838e84f6df7a2f9504ad979b19b972fbf4d3119
Tolley <chris.tolley@gmail.com>
2025-08-03 14:11:06 -0400

    Modified the Sequelize connection for secure connections

--- models/sequelize_initialize.js
+++ models/sequelize_initialize.js
@@ -1,12 +1,24 @@
 var Sequelize = require( 'sequelize' );
 
+var sequelizeOpts = {
+	host: process.env.MYSQL_HOST,
+	dialect: 'mysql',
+	logging: false
+};
+
+if( process.env.ENV == 'production' ) {
+	sequelizeOpts.ssl = true;
+	sequelizeOpts.dialectOptions = {
+		ssl: {
+			ca: fs.readFileSync( '/root/mysql_cert.pem'  ).toString()
+		}
+	}
+}
+
+
 module.exports = new Sequelize( 
 	process.env.MYSQL_DATABASE,
 	process.env.MYSQL_USER,
 	process.env.MYSQL_PASSWORD,
-	{
-		host: process.env.MYSQL_HOST,
-		dialect: 'mysql',
-		logging: false
-	}
+	sequelizeOpts
 );
